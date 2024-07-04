c2bba14dbb4df0be98824b0b3b63806a345fb5e7
Kaery <64017775+Kaery40@users.noreply.github.com>
2024-07-04 12:19:46 +0800

    Update main.js

--- main.js
+++ main.js
@@ -160,7 +160,7 @@ class Home {
 				}
 			}, 16);
 		});
-		
+
 		// 判断section0加载完毕
 		await new Promise((resolve, reject) => {
 			let waitsection0 = setInterval(() => {
@@ -170,15 +170,17 @@ class Home {
 				}
 			}, 16);
 		});
-		
+
 		$(".view:not(.hide) .section0 .emby-scrollbuttons").remove();
 		const items = $(".view:not(.hide) .section0 .emby-scroller .itemsContainer")[0].items;
-		$(".view:not(.hide) .section0").detach().appendTo(".view:not(.hide) .misty-banner-library");
-		
+		if (CommonUtils.checkType() === 'pc') {
+			$(".view:not(.hide) .section0").detach().appendTo(".view:not(.hide) .misty-banner-library");
+		}
+
 		$(".misty-loading").fadeOut(500, () => $(".misty-loading").remove());
 		await CommonUtils.sleep(150);
 		$(".view:not(.hide) .section0 .emby-scroller .itemsContainer")[0].items = items;
-		
+
 		// 置入场动画
 		let delay = 80; // 动媒体库画间隔
 		let id = $(".misty-banner-item").eq(0).addClass("active").attr("id"); // 初次信息动画
